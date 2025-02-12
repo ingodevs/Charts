9347bb6cd8915b6e3d62d8f00219fe4a6da7ddab
Doris Darwich <doris.darwich@cmeoffshore.com>
2025-02-12 16:51:03 +0200

    Gave bars corner Radius in bar chart

--- Source/Charts/Renderers/BarChartRenderer.swift
+++ Source/Charts/Renderers/BarChartRenderer.swift
@@ -351,7 +351,9 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 guard viewPortHandler.isInBoundsRight(barRect.origin.x) else { break }
 
                 context.setFillColor(dataSet.barShadowColor.cgColor)
-                context.fill(barRect)
+                let path = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 5, height: 5))
+                context.addPath(path.cgPath)
+                context.fillPath()
             }
         }
         
@@ -379,7 +381,9 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 context.setFillColor(dataSet.color(atIndex: j).cgColor)
             }
             
-            context.fill(barRect)
+            let path = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 5, height: 5))
+            context.addPath(path.cgPath)
+            context.fillPath()
             
             if drawBorder
             {
@@ -744,7 +748,9 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 
                 setHighlightDrawPos(highlight: high, barRect: barRect)
                 
-                context.fill(barRect)
+                let path = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 5, height: 5))
+                context.addPath(path.cgPath)
+                context.fillPath()
             }
         }
     }
